032b62d26137d96fd2c162920499af1f2153de88
vishnu-vk619 <75382750+vishnu-vk619@users.noreply.github.com>
2022-06-20 20:34:31 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-customhost = "employee.coghw13fheqo.us-east-2.rds.amazonaws.com"
-customuser = "intellipaat"
-custompass = "intellipaat123"
-customdb = "employee"
-custombucket = "addemployee"
-customregion = "us-east-2"
