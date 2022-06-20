a510e561b947ba9c066d088b8156731557ed78f5
vishnu-vk619 <75382750+vishnu-vk619@users.noreply.github.com>
2022-06-20 20:34:57 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cujavq47oj5k.us-east-1.rds.amazonaws.com"
+customuser = "intellipaat"
+custompass = "intellipaat123"
+customdb = "employee"
+custombucket = "addemployee07"
+customregion = "us-east-1"
